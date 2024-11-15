7ab625efc46f2a58cdcfa62d79f38021c4d36106
KIM YUEN <kim_man@live.com>
2024-11-15 17:55:56 +1100

    refactor: create a custom hook useJobItems

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Background from './Background';
 import Container from './Container';
 import Footer from './Footer';
@@ -12,25 +12,11 @@ import PaginationControls from './PaginationControls';
 import JobList from './JobList';
 import SortingControls from './SortingControls';
 import ResultsCount from './ResultsCount';
+import { useJobItems } from '../lib/hooks';
 
 function App() {
-  const [jobItems, setJobItems] = useState([]);
-  const [searchText, setSearchText] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!searchText) return;
-    const fetchData = async () => {
-      setIsLoading(true);
-      const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-      );
-      const data = await response.json();
-      setJobItems(data.jobItems);
-      setIsLoading(false);
-    };
-    fetchData();
-  }, [searchText]);
+  const [searchText, setSearchText] = useState([]);
+  const { jobItems, isLoading } = useJobItems(searchText);
   return (
     <>
       <Background />
---
+++ src/lib/hooks.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+export function useJobItems(searchText: string) {
+  const [searchText, setSearchText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!searchText) return;
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+      );
+      const data = await response.json();
+      setJobItems(data.jobItems);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [searchText]);
+  return { jobItems, isLoading };
+}
