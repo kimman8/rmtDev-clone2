e95d818c07884d000653d38e707fde5e7cd0355c
KIM YUEN <kim_man@live.com>
2024-12-02 17:12:22 +1100

    refactor: return object instead of array in the custom hooks

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -33,7 +33,7 @@ export function useJobItem(id: number | null) {
     };
     fetchData();
   }, [id]);
-  return [jobItem, isLoading] as const;
+  return { jobItem, isLoading } as const;
 }
 
 export function useJobItems(searchText: string) {
@@ -53,5 +53,5 @@ export function useJobItems(searchText: string) {
     };
     fetchData();
   }, [searchText]);
-  return [jobItemsSliced, isLoading, totalNumberOfResults] as const;
+  return { jobItemsSliced, isLoading, totalNumberOfResults } as const;
 }
