2de21ed63592714a22d896d560de9dea079d820a
KIM YUEN <kim_man@live.com>
2024-12-16 12:29:56 +1100

    refactor: refactor the data fetching to react-query

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,44 +1,28 @@
 import { useEffect, useState } from 'react';
-import { JobItem, JobItemExpanded } from './types';
+import { JobItem } from './types';
 import { BASE_API_URL } from './constants';
 import { useQuery } from '@tanstack/react-query';
 
-export function useActiveId() {
-  const [activeId, setActiveId] = useState<number | null>(null);
-  useEffect(() => {
-    const handleHashChange = () => {
-      const id = +window.location.hash.slice(1);
-      setActiveId(id);
-    };
-    handleHashChange();
-    window.addEventListener('hashchange', handleHashChange);
-    return () => {
-      window.removeEventListener('hashchange', handleHashChange);
-    };
-  }, []);
-  return activeId;
-}
-
-// export function useJobItem(id: number | null) {
-//   const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     if (!id) return;
-//     const fetchData = async () => {
-//       setIsLoading(true);
-//       const response = await fetch(`${BASE_API_URL}/${id}`);
-//       const data = await response.json();
-//       setJobItem(data.jobItem);
-//       setIsLoading(false);
-//     };
-//     fetchData();
-//   }, [id]);
-//   return { jobItem, isLoading } as const;
-// }
-
 export function useJobItem(id: number | null) {
-  const { data, isLoading } = useQuery();
+  const { data, isLoading } = useQuery(
+    ['jobItem', id],
+    async () => {
+      const response = await fetch(`${BASE_API_URL}/${id}`);
+      const data = await response.json();
+      return data;
+    },
+    {
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: Boolean(id),
+      onError: (error) => {
+        console.error(error);
+      },
+    }
+  );
+  const jobItem = data.jobItem;
+  return { jobItem, isLoading } as const;
 }
 
 export function useJobItems(searchText: string) {
@@ -68,4 +52,20 @@ export function useDebounce<T>(value: T, delay = 1000): T {
     return () => clearTimeout(timerId);
   }, [value, delay]);
   return debouncedValue;
+}
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleHashChange();
+    window.addEventListener('hashchange', handleHashChange);
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
+  return activeId;
 }
